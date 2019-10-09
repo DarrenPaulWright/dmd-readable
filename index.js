cd266e29b69578d3c35ff1ea22e9e5243e3316fd
DarrenPaulWright <darren@darrenpaulwright.com>
2019-10-08 22:52:03 -0700

    Changing global index removal to a setting, updating docs.

--- index.js
+++ index.js
@@ -10,20 +10,28 @@
 /**
  * @name Usage
  * @summary
- * Pass the plug-in name to `jsdoc2md` or `dmd`:
+ * Pass the plug-in name to [`jsdoc2md`](https://github.com/jsdoc2md/jsdoc-to-markdown) or [`dmd`](https://github.com/jsdoc2md/dmd):
  *
  * ```
  * jsdoc2md --plugin dmd-readable
  * ```
  *
  * This plugin (which was used to generate this readme) does a few things:
- * - removes global indexes
- * - places descriptions in block-quotes
+ * - removes global indexes (see below)
+ * - places descriptions in block-quotes (Use @summary for descriptions without block quotes)
  * - adds more whitespace before headings
  * - changes the delimiter for multiple types in param tables to a comma
  * - adds alias output
  *
- * Use @summary for descriptions without block quotes.
+ * The removal of global indexes is now a setting in the [template](docs.hbs). If you are using your own template and you wish to retain this feature, add the following line to your template:
+ *
+ * ```
+ * {{optionSet "global-index-format" "none"~}}
+ * ```
+ *
+ * All options for global-index-format are "none", "grouped", "table", "dl". Other options and more info can be found [here](https://github.com/jsdoc2md/jsdoc-to-markdown/blob/master/docs/API.md#jsdoctomarkdown-).
+ *
+ * This plugin also provides the following helper functions:
  *
  */
 module.exports = function() {
